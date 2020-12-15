5b8db8667cc2d136c2d93cd6e53130ad77c113bf
Mehmood <Saad.Mehmood@corp.ibexglobal.com>
2020-12-15 21:23:39 +0500

    Fist Commit

---
+++ src/test/java/com/devtoolsdemo/DevToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtoolsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
